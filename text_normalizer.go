94f671000dd36d94ed54506b93b9e506311c9653
tomoemon <bach48+github_tomoemon@gmail.com>
2018-11-30 12:17:21 +0900

    add TextNormalizer

---
+++ text_normalizer.go
@@ -0,0 +1,391 @@
+package text_normalizer
+
+import "strings"
+
+type TextReplaceOption int
+
+/*
+用語の定義
+Hankaku…半角文字
+Zenkaku…全角文字
+Katakana…カタカナ
+Hiragana…ひらがな
+Kana…カタカナ、ひらがな
+Alphabet…英字（A-Z）
+Sign…記号
+Space…空白文字
+*/
+
+const (
+	// 以下、変換プロセスに影響を与える共通オプション
+
+	// 濁点なしの文字に変換する（カナ系文字への変換オプションと同時に使う）
+	RemoveDakuten TextReplaceOption = iota + 1
+
+	// 変換先の文字が存在しない場合に削除する（デフォルト：無視する）
+	RemoveNoMapping
+
+	// 以下、個別の変換オプション
+
+	// 半角数字→全角数字
+	HankakuNumberToZenkaku
+
+	// 全角数字→半角数字
+	ZenkakuNumberToHankaku
+
+	// 半角カタカナ→全角カタカナ
+	HankakuKatakanaToZenkaku
+
+	// 全角カタカナ→半角カタカナ
+	ZenkakuKatakanaToHankaku
+
+	// (全角カタカナ、半角カタカナ）→ひらがな
+	KatakanaToHiragana
+
+	// ひらがな→全角カタカナ
+	HiraganaToZenkakuKatakana
+
+	// ひらがな→半角カタカナ
+	HiraganaToHankakuKatakana
+
+	// (ひらがな、全角カタカナ、半角カタカナ) →ひらがな
+	KanaToHiragana
+
+	// (ひらがな、全角カタカナ、半角カタカナ) →全角カタカナ
+	KanaToZenkakuKatakana
+
+	// (ひらがな、全角カタカナ、半角カタカナ) →半角カタカナ
+	KanaToHankakuKatakana
+
+	// (半角英字、全角英字) →全角大文字英字
+	AlphabetToUpperZenkaku
+
+	// (半角英字、全角英字) →半角大文字英字
+	AlphabetToUpperHankaku
+
+	// (半角英字、全角英字) →全角小文字英字
+	AlphabetToLowerZenkaku
+
+	// (半角英字、全角英字) →半角小文字英字
+	AlphabetToLowerHankaku
+
+	// (半角英字、全角英字) →全角英字（大文字小文字の区別は維持する）
+	AlphabetToZenkaku
+
+	// (半角英字、全角英字) →半角英字（大文字小文字の区別は維持する）
+	AlphabetToHankaku
+
+	// 半角記号→全角記号
+	HankakuSignToZenkaku
+
+	// 全角記号→半角記号
+	ZenkakuSignToHankaku
+
+	// 半角スペース→全角スペース
+	HankakuSpaceToZenkaku
+
+	// 全角スペース→半角スペース
+	ZenkakuSpaceToHankaku
+)
+
+func hasOption(flags []TextReplaceOption, search TextReplaceOption) bool {
+	for _, f := range flags {
+		if f == search {
+			return true
+		}
+	}
+	return false
+}
+
+func NewTextNormalizer(flags ...TextReplaceOption) *strings.Replacer {
+	noDakutenSlide := 0
+	if hasOption(flags, RemoveDakuten) {
+		noDakutenSlide = 3
+	}
+	removeNoMapping := false
+	if hasOption(flags, RemoveNoMapping) {
+		removeNoMapping = true
+	}
+
+	projectAsciiNumber := AsciiNumberMap.projectFuncWithOption(removeNoMapping)
+	projectKana := JapaneseKanaMap.projectFuncWithOption(removeNoMapping)
+	projectAsciiAlphabet := AsciiAlphabetMap.projectFuncWithOption(removeNoMapping)
+	projectAsciiSign := AsciiSignMap.projectFuncWithOption(removeNoMapping)
+	projectSpace := SpaceMap.projectFuncWithOption(removeNoMapping)
+
+	var replacerMapping []string
+	for _, f := range flags {
+		switch f {
+		case HankakuNumberToZenkaku:
+			replacerMapping = append(replacerMapping, projectAsciiNumber(0, 1)...)
+		case ZenkakuNumberToHankaku:
+			replacerMapping = append(replacerMapping, projectAsciiNumber(1, 0)...)
+		case HankakuKatakanaToZenkaku:
+			replacerMapping = append(replacerMapping, projectKana(0, 1+noDakutenSlide)...)
+		case ZenkakuKatakanaToHankaku:
+			replacerMapping = append(replacerMapping, projectKana(1, 0+noDakutenSlide)...)
+		case KatakanaToHiragana:
+			replacerMapping = append(replacerMapping, projectKana(0, 2+noDakutenSlide)...)
+			replacerMapping = append(replacerMapping, projectKana(1, 2+noDakutenSlide)...)
+		case HiraganaToZenkakuKatakana:
+			replacerMapping = append(replacerMapping, projectKana(2, 1+noDakutenSlide)...)
+		case HiraganaToHankakuKatakana:
+			replacerMapping = append(replacerMapping, projectKana(2, 0+noDakutenSlide)...)
+		case KanaToHankakuKatakana:
+			replacerMapping = append(replacerMapping, projectKana(0, 0+noDakutenSlide)...)
+			replacerMapping = append(replacerMapping, projectKana(1, 0+noDakutenSlide)...)
+			replacerMapping = append(replacerMapping, projectKana(2, 0+noDakutenSlide)...)
+		case KanaToZenkakuKatakana:
+			replacerMapping = append(replacerMapping, projectKana(0, 1+noDakutenSlide)...)
+			replacerMapping = append(replacerMapping, projectKana(1, 1+noDakutenSlide)...)
+			replacerMapping = append(replacerMapping, projectKana(2, 1+noDakutenSlide)...)
+		case KanaToHiragana:
+			replacerMapping = append(replacerMapping, projectKana(0, 2+noDakutenSlide)...)
+			replacerMapping = append(replacerMapping, projectKana(1, 2+noDakutenSlide)...)
+			replacerMapping = append(replacerMapping, projectKana(2, 2+noDakutenSlide)...)
+		case AlphabetToUpperZenkaku:
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(0, 3)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(1, 3)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(2, 3)...)
+		case AlphabetToUpperHankaku:
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(0, 1)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(2, 1)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(3, 1)...)
+		case AlphabetToLowerZenkaku:
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(0, 2)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(1, 2)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(3, 2)...)
+		case AlphabetToLowerHankaku:
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(1, 0)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(2, 0)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(3, 0)...)
+		case AlphabetToZenkaku:
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(0, 2)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(1, 3)...)
+		case AlphabetToHankaku:
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(2, 0)...)
+			replacerMapping = append(replacerMapping, projectAsciiAlphabet(3, 1)...)
+		case HankakuSignToZenkaku:
+			replacerMapping = append(replacerMapping, projectAsciiSign(0, 1)...)
+		case ZenkakuSignToHankaku:
+			replacerMapping = append(replacerMapping, projectAsciiSign(1, 0)...)
+		case HankakuSpaceToZenkaku:
+			replacerMapping = append(replacerMapping, projectSpace(0, 1)...)
+		case ZenkakuSpaceToHankaku:
+			replacerMapping = append(replacerMapping, projectSpace(1, 0)...)
+		}
+	}
+	return strings.NewReplacer(replacerMapping...)
+}
+
+type charMap struct {
+	data [][]string
+}
+
+func (c *charMap) projectFuncWithOption(removeUnknown bool) func(int, int) []string {
+	return func(from, to int) []string {
+		return c.project(from, to, removeUnknown)
+	}
+}
+
+func (c *charMap) project(from, to int, removeUnknown bool) []string {
+	if len(c.data) == 0 {
+		return nil
+	}
+	var result []string
+	for _, cols := range c.data {
+		v1 := cols[from]
+		v2 := cols[to]
+		if v1 == "" {
+			continue
+		}
+		if v2 == "" && !removeUnknown {
+			v2 = v1
+		}
+		result = append(result, v1, v2)
+	}
+	return result
+}
+
+var AsciiNumberMap = charMap{
+	data: [][]string{
+		// 半角数字, 全角数字
+		{"0", "０"},
+		{"1", "１"},
+		{"2", "２"},
+		{"3", "３"},
+		{"4", "４"},
+		{"5", "５"},
+		{"6", "６"},
+		{"7", "７"},
+		{"8", "８"},
+		{"9", "９"},
+	},
+}
+
+var JapaneseKanaMap = charMap{
+	data: [][]string{
+		// 半角カタカナ, 全角カタカナ, 全角ひらがな、濁点なし半角カタカナ、濁点なし全角カタカナ、濁点なし全角ひらがな
+		// マッピングは上にあるものから順に適用される
+		{"ｶﾞ", "ガ", "が", "ｶ", "カ", "か"},
+		{"ｷﾞ", "ギ", "ぎ", "ｷ", "キ", "き"},
+		{"ｸﾞ", "グ", "ぐ", "ｸ", "ク", "く"},
+		{"ｹﾞ", "ゲ", "げ", "ｹ", "ケ", "け"},
+		{"ｺﾞ", "ゴ", "ご", "ｺ", "コ", "こ"},
+		{"ｻﾞ", "ザ", "ざ", "ｻ", "サ", "さ"},
+		{"ｼﾞ", "ジ", "じ", "ｼ", "シ", "し"},
+		{"ｽﾞ", "ズ", "ず", "ｽ", "ス", "す"},
+		{"ｾﾞ", "ゼ", "ぜ", "ｾ", "セ", "せ"},
+		{"ｿﾞ", "ゾ", "ぞ", "ｿ", "ソ", "そ"},
+		{"ﾀﾞ", "ダ", "だ", "ﾀ", "タ", "た"},
+		{"ﾁﾞ", "ヂ", "ぢ", "ﾁ", "チ", "ち"},
+		{"ﾂﾞ", "ヅ", "づ", "ﾂ", "ツ", "つ"},
+		{"ﾃﾞ", "デ", "で", "ﾃ", "テ", "て"},
+		{"ﾄﾞ", "ド", "ど", "ﾄ", "ト", "と"},
+		{"ﾊﾞ", "バ", "ば", "ﾊ", "ハ", "は"},
+		{"ﾋﾞ", "ビ", "び", "ﾋ", "ヒ", "ひ"},
+		{"ﾌﾞ", "ブ", "ぶ", "ﾌ", "フ", "ふ"},
+		{"ﾍﾞ", "ベ", "べ", "ﾍ", "へ", "へ"},
+		{"ﾎﾞ", "ボ", "ぼ", "ﾎ", "ホ", "ほ"},
+		{"ﾊﾟ", "パ", "ぱ", "ﾊ", "ハ", "は"},
+		{"ﾋﾟ", "ピ", "ぴ", "ﾋ", "ヒ", "ひ"},
+		{"ﾌﾟ", "プ", "ぷ", "ﾌ", "フ", "ふ"},
+		{"ﾍﾟ", "ペ", "ぺ", "ﾍ", "ヘ", "へ"},
+		{"ﾎﾟ", "ポ", "ぽ", "ﾎ", "ホ", "ほ"},
+		{"ｳﾞ", "ヴ", "ゔ", "ｳ", "ウ", "う"},
+		{"ﾜﾞ", "ヷ", "", "ﾜ", "ワ", "わ"},
+		{"ｦﾞ", "ヺ", "", "ｦ", "ヲ", "を"},
+		{"ｱ", "ア", "あ", "ｱ", "ア", "あ"},
+		{"ｲ", "イ", "い", "ｲ", "イ", "い"},
+		{"ｳ", "ウ", "う", "ｳ", "ウ", "う"},
+		{"ｴ", "エ", "え", "ｴ", "エ", "え"},
+		{"ｵ", "オ", "お", "ｵ", "オ", "お"},
+		{"ｶ", "カ", "か", "ｶ", "カ", "か"},
+		{"ｷ", "キ", "き", "ｷ", "キ", "き"},
+		{"ｸ", "ク", "く", "ｸ", "ク", "く"},
+		{"ｹ", "ケ", "け", "ｹ", "ケ", "け"},
+		{"ｺ", "コ", "こ", "ｺ", "コ", "こ"},
+		{"ｻ", "サ", "さ", "ｻ", "サ", "さ"},
+		{"ｼ", "シ", "し", "ｼ", "シ", "し"},
+		{"ｽ", "ス", "す", "ｽ", "ス", "す"},
+		{"ｾ", "セ", "せ", "ｾ", "セ", "せ"},
+		{"ｿ", "ソ", "そ", "ｿ", "ソ", "そ"},
+		{"ﾀ", "タ", "た", "ﾀ", "タ", "た"},
+		{"ﾁ", "チ", "ち", "ﾁ", "チ", "ち"},
+		{"ﾂ", "ツ", "つ", "ﾂ", "ツ", "つ"},
+		{"ﾃ", "テ", "て", "ﾃ", "テ", "て"},
+		{"ﾄ", "ト", "と", "ﾄ", "ト", "と"},
+		{"ﾅ", "ナ", "な", "ﾅ", "ナ", "な"},
+		{"ﾆ", "ニ", "に", "ﾆ", "ニ", "に"},
+		{"ﾇ", "ヌ", "ぬ", "ﾇ", "ヌ", "ぬ"},
+		{"ﾈ", "ネ", "ね", "ﾈ", "ネ", "ね"},
+		{"ﾉ", "ノ", "の", "ﾉ", "ノ", "の"},
+		{"ﾊ", "ハ", "は", "ﾊ", "ハ", "は"},
+		{"ﾋ", "ヒ", "ひ", "ﾋ", "ヒ", "ひ"},
+		{"ﾌ", "フ", "ふ", "ﾌ", "フ", "ふ"},
+		{"ﾍ", "ヘ", "へ", "ﾍ", "ヘ", "へ"},
+		{"ﾎ", "ホ", "ほ", "ﾎ", "ホ", "ほ"},
+		{"ﾏ", "マ", "ま", "ﾏ", "マ", "ま"},
+		{"ﾐ", "ミ", "み", "ﾐ", "ミ", "み"},
+		{"ﾑ", "ム", "む", "ﾑ", "ム", "む"},
+		{"ﾒ", "メ", "め", "ﾒ", "メ", "め"},
+		{"ﾓ", "モ", "も", "ﾓ", "モ", "も"},
+		{"ﾔ", "ヤ", "や", "ﾔ", "ヤ", "や"},
+		{"ﾕ", "ユ", "ゆ", "ﾕ", "ユ", "ゆ"},
+		{"ﾖ", "ヨ", "よ", "ﾖ", "ヨ", "よ"},
+		{"ﾗ", "ラ", "ら", "ﾗ", "ラ", "ら"},
+		{"ﾘ", "リ", "り", "ﾘ", "リ", "り"},
+		{"ﾙ", "ル", "る", "ﾙ", "ル", "る"},
+		{"ﾚ", "レ", "れ", "ﾚ", "レ", "れ"},
+		{"ﾛ", "ロ", "ろ", "ﾛ", "ロ", "ろ"},
+		{"ﾜ", "ワ", "わ", "ﾜ", "ワ", "わ"},
+		{"ｦ", "ヲ", "を", "ｦ", "ヲ", "を"},
+		{"ﾝ", "ン", "ん", "ﾝ", "ン", "ん"},
+		{"ｧ", "ァ", "ぁ", "ｧ", "ァ", "ぁ"},
+		{"ｨ", "ィ", "ぃ", "ｨ", "ィ", "ぃ"},
+		{"ｩ", "ゥ", "ぅ", "ｩ", "ゥ", "ぅ"},
+		{"ｪ", "ェ", "ぇ", "ｪ", "ェ", "ぇ"},
+		{"ｫ", "ォ", "ぉ", "ｫ", "ォ", "ぉ"},
+		{"ｯ", "ッ", "っ", "ｯ", "ッ", "っ"},
+		{"ｬ", "ャ", "ゃ", "ｬ", "ャ", "ゃ"},
+		{"ｭ", "ュ", "ゅ", "ｭ", "ュ", "ゅ"},
+		{"ｮ", "ョ", "ょ", "ｮ", "ョ", "ょ"},
+	},
+}
+
+var AsciiAlphabetMap = charMap{
+	data: [][]string{
+		// 半角小文字, 半角大文字, 全角小文字, 全角大文字
+		{"a", "A", "ａ", "Ａ"},
+		{"b", "B", "ｂ", "Ｂ"},
+		{"c", "C", "ｃ", "Ｃ"},
+		{"d", "D", "ｄ", "Ｄ"},
+		{"e", "E", "ｅ", "Ｅ"},
+		{"f", "F", "ｆ", "Ｆ"},
+		{"g", "G", "ｇ", "Ｇ"},
+		{"h", "H", "ｈ", "Ｈ"},
+		{"i", "I", "ｉ", "Ｉ"},
+		{"j", "J", "ｊ", "Ｊ"},
+		{"k", "K", "ｋ", "Ｋ"},
+		{"l", "L", "ｌ", "Ｌ"},
+		{"m", "M", "ｍ", "Ｍ"},
+		{"n", "N", "ｎ", "Ｎ"},
+		{"o", "O", "ｏ", "Ｏ"},
+		{"p", "P", "ｐ", "Ｐ"},
+		{"q", "Q", "ｑ", "Ｑ"},
+		{"r", "R", "ｒ", "Ｒ"},
+		{"s", "S", "ｓ", "Ｓ"},
+		{"t", "T", "ｔ", "Ｔ"},
+		{"u", "U", "ｕ", "Ｕ"},
+		{"v", "V", "ｖ", "Ｖ"},
+		{"w", "W", "ｗ", "Ｗ"},
+		{"x", "X", "ｘ", "Ｘ"},
+		{"y", "Y", "ｙ", "Ｙ"},
+		{"z", "Z", "ｚ", "Ｚ"},
+	},
+}
+
+var SpaceMap = charMap{
+	data: [][]string{
+		// 半角スペース, 全角スペース
+		{" ", "　"},
+	},
+}
+
+var AsciiSignMap = charMap{
+	data: [][]string{
+		// 半角記号, 全角記号
+		{"!", "！"},
+		{`"`, `”`},
+		{"#", "＃"},
+		{"$", "＄"},
+		{"%", "％"},
+		{"&", "＆"},
+		{"'", "’"},
+		{"(", "（"},
+		{")", "）"},
+		{"*", "＊"},
+		{"+", "＋"},
+		{",", "、"},
+		{"-", "－"},
+		{".", "。"},
+		{"/", "・"},
+		{":", "："},
+		{";", "；"},
+		{"<", "＜"},
+		{"=", "＝"},
+		{">", "＞"},
+		{"?", "？"},
+		{"@", "＠"},
+		{"[", "「"},
+		{"\\", "￥"},
+		{"]", "」"},
+		{"^", "＾"},
+		{"_", "＿"},
+		{"`", "｀"},
+		{"{", "｛"},
+		{"|", "｜"},
+		{"}", "｝"},
+		{"~", "～"},
+	},
+}
---
+++ text_normalizer_test.go
@@ -0,0 +1,205 @@
+package text_normalizer
+
+import (
+	"testing"
+)
+
+func TestNewNormalizer(t *testing.T) {
+	tests := []struct {
+		options []TextReplaceOption
+		name    string
+		input   string
+		want    string
+	}{
+		{
+			name:    "半角数字を全角数字に変換",
+			options: []TextReplaceOption{HankakuNumberToZenkaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ１２３！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC１２３!#$ａｂｃＡＢＣ１２３！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+		},
+		{
+			name:    "全角数字を半角数字に変換",
+			options: []TextReplaceOption{ZenkakuNumberToHankaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ１２３！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+		},
+		{
+			name:    "半角カタカナを全角カタカナに変換",
+			options: []TextReplaceOption{HankakuKatakanaToZenkaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄アイウアイウあいうガガがパパぱ' '　'",
+		},
+		{
+			name:    "半角カタカナを濁点なし全角カタカナに変換",
+			options: []TextReplaceOption{HankakuKatakanaToZenkaku, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄アイウアイウあいうカガがハパぱ' '　'",
+		},
+		{
+			name:    "全角カタカナを半角カタカナに変換",
+			options: []TextReplaceOption{ZenkakuKatakanaToHankaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳｱｲｳあいうｶﾞｶﾞがﾊﾟﾊﾟぱ' '　'",
+		},
+		{
+			name:    "全角カタカナを濁点なし半角カタカナに変換",
+			options: []TextReplaceOption{ZenkakuKatakanaToHankaku, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳｱｲｳあいうｶﾞｶがﾊﾟﾊぱ' '　'",
+		},
+		{
+			name:    "全角、半角カタカナをひらがなに変換",
+			options: []TextReplaceOption{KatakanaToHiragana},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄あいうあいうあいうがががぱぱぱ' '　'",
+		},
+		{
+			name:    "全角、半角カタカナを濁点なしひらがなに変換",
+			options: []TextReplaceOption{KatakanaToHiragana, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄あいうあいうあいうかかがははぱ' '　'",
+		},
+		{
+			name:    "ひらがなを全角カタカナに変換",
+			options: []TextReplaceOption{HiraganaToZenkakuKatakana},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウアイウｶﾞガガﾊﾟパパ' '　'",
+		},
+		{
+			name:    "ひらがなを濁点なし全角カタカナに変換",
+			options: []TextReplaceOption{HiraganaToZenkakuKatakana, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウアイウｶﾞガカﾊﾟパハ' '　'",
+		},
+		{
+			name:    "ひらがなを半角カタカナに変換",
+			options: []TextReplaceOption{HiraganaToHankakuKatakana},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウｱｲｳｶﾞガｶﾞﾊﾟパﾊﾟ' '　'",
+		},
+		{
+			name:    "ひらがなを濁点なし半角カタカナに変換",
+			options: []TextReplaceOption{HiraganaToHankakuKatakana, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウｱｲｳｶﾞガｶﾊﾟパﾊ' '　'",
+		},
+		{
+			name:    "全角、半角カタカナ、ひらがなを半角カタカナに変換",
+			options: []TextReplaceOption{KanaToHankakuKatakana},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳｱｲｳｱｲｳｶﾞｶﾞｶﾞﾊﾟﾊﾟﾊﾟ' '　'",
+		},
+		{
+			name:    "全角、半角カタカナ、ひらがなを濁点なし半角カタカナに変換",
+			options: []TextReplaceOption{KanaToHankakuKatakana, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳｱｲｳｱｲｳｶｶｶﾊﾊﾊ' '　'",
+		},
+		{
+			name:    "全角、半角カタカナ、ひらがなを全角カタカナに変換",
+			options: []TextReplaceOption{KanaToZenkakuKatakana},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄アイウアイウアイウガガガパパパ' '　'",
+		},
+		{
+			name:    "全角、半角カタカナ、ひらがなを濁点なし全角カタカナに変換",
+			options: []TextReplaceOption{KanaToZenkakuKatakana, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄アイウアイウアイウカカカハハハ' '　'",
+		},
+		{
+			name:    "全角、半角カタカナ、ひらがなをひらがなに変換",
+			options: []TextReplaceOption{KanaToHiragana},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄あいうあいうあいうがががぱぱぱ' '　'",
+		},
+		{
+			name:    "全角、半角カタカナ、ひらがなを濁点なしひらがなに変換",
+			options: []TextReplaceOption{KanaToHiragana, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123！＃＄あいうあいうあいうかかかははは' '　'",
+		},
+		{
+			name:    "英字を全角大文字に変換",
+			options: []TextReplaceOption{AlphabetToUpperZenkaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "ＡＢＣＡＢＣ123!#$ＡＢＣＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+		},
+		{
+			name:    "英字を半角大文字に変換",
+			options: []TextReplaceOption{AlphabetToUpperHankaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "ABCABC123!#$ABCABC123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+		},
+		{
+			name:    "英字を全角小文字に変換",
+			options: []TextReplaceOption{AlphabetToLowerZenkaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "ａｂｃａｂｃ123!#$ａｂｃａｂｃ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+		},
+		{
+			name:    "英字を半角小文字に変換",
+			options: []TextReplaceOption{AlphabetToLowerHankaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+			want:    "abcabc123!#$abcabc123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ' '　'",
+		},
+		{
+			name:    "英字を全角に変換",
+			options: []TextReplaceOption{AlphabetToZenkaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+			want:    "ａｂｃＡＢＣ123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+		},
+		{
+			name:    "英字を半角に変換",
+			options: []TextReplaceOption{AlphabetToHankaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+			want:    "abcABC123!#$abcABC123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+		},
+		{
+			name:    "半角記号を全角に変換",
+			options: []TextReplaceOption{HankakuSignToZenkaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+			want:    "abcABC123！＃＄ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+		},
+		{
+			name:    "全角記号を半角に変換",
+			options: []TextReplaceOption{ZenkakuSignToHankaku},
+			input:   "abcABC123!#$ａｂｃＡＢＣ123！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+			want:    "abcABC123!#$ａｂｃＡＢＣ123!#$ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+		},
+		{
+			name:    "全角数字を半角に、全角記号を半角に、英字を半角小文字に、かな文字を全角カタカナに変換",
+			options: []TextReplaceOption{ZenkakuNumberToHankaku, ZenkakuSignToHankaku, AlphabetToLowerHankaku, KanaToZenkakuKatakana},
+			input:   "abcABC123!#$ａｂｃＡＢＣ１２３！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+			want:    "abcabc123!#$abcabc123!#$アイウアイウアイウガガガパパパ",
+		},
+		{
+			name:    "全角数字を半角に、全角記号を半角に、英字を半角小文字に、かな文字を濁点なし全角カタカナに変換",
+			options: []TextReplaceOption{ZenkakuNumberToHankaku, ZenkakuSignToHankaku, AlphabetToLowerHankaku, KanaToZenkakuKatakana, RemoveDakuten},
+			input:   "abcABC123!#$ａｂｃＡＢＣ１２３！＃＄ｱｲｳアイウあいうｶﾞガがﾊﾟパぱ",
+			want:    "abcabc123!#$abcabc123!#$アイウアイウアイウカカカハハハ",
+		},
+		{
+			name:    "マッピング先がないものは変換時に無視される",
+			options: []TextReplaceOption{KanaToHiragana},
+			input:   "アイウﾜﾞヷ",
+			want:    "あいうﾜﾞヷ",
+		},
+		{
+			name:    "マッピング先がないものは変換時に消される",
+			options: []TextReplaceOption{KanaToHiragana, RemoveNoMapping},
+			input:   "アイウﾜﾞヷ",
+			want:    "あいう",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			n := NewTextNormalizer(test.options...)
+			got := n.Replace(test.input)
+			if test.want != got {
+				t.Errorf("want: %s, got: %s (options: %+v)", test.want, got, test.options)
+			}
+		})
+	}
+}
